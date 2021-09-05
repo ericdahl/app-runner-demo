fa8cc0a9293c17ca7e818353d2a813a84cde434b
Eric Dahl <eric.c.dahl@gmail.com>
2021-09-05 19:47:34 +0000

    clean

--- app.py
+++ app.py
@@ -2,12 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 
 
-print("hello")
-
 @app.route("/")
 def index():
-    return "<h1 style='color:blue'>Hello There!</h1>"
-
-# if __name__ == "__main__":
-#     print("tryign to run")
-#     app.run(host='0.0.0.0', port=8080)
+    return "hello world"
